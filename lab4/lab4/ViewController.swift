462debda64b2eb420178311c20263dc96762616a
Naitik59 <123869903+Naitik59@users.noreply.github.com>
2024-06-02 21:29:33 -0400

    another commit
    another commit

--- lab4/lab4/ViewController.swift
+++ lab4/lab4/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mylabel: UILabel!
-    
     @IBOutlet weak var firstName: UITextField!
-    
     @IBOutlet weak var surname: UITextField!
-    
     @IBOutlet weak var city: UITextField!
-    
     @IBOutlet weak var address: UITextField!
-    
     @IBOutlet weak var dateOfBirth: UITextField!
+    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +28,8 @@ class ViewController: UIViewController {
         city.text = ""
         address.text = ""
         dateOfBirth.text = ""
+        textView.text = ""
+        mylabel.text = "Please fill the form"
     }
     
     @IBAction func decline(_ sender: UIButton) {
@@ -41,7 +39,7 @@ class ViewController: UIViewController {
         address.text = ""
         dateOfBirth.text = ""
         mylabel.text = "user has declined"
-        
+        textView.text = ""
     }
     
     @IBAction func accept(_ sender: UIButton) {
@@ -50,13 +48,20 @@ class ViewController: UIViewController {
         }
         else {
             let dateFormater = DateFormatter()
-            dateFormater.dateFormat = "MM/dd/yyyy"
-            let birthDate = dateFormater.date(from: dateOfBirth.text!)
-            
-            let calender = Calendar.current
-            let age = calender.dateComponents([.year], from: birthDate!, to: Date())
-            
-            print(age.year)
+            dateFormater.dateFormat = "dd/MM/yyyy"
+            if let birthDate = dateFormater.date(from: dateOfBirth.text!) {
+                let calender = Calendar.current
+                let age = calender.dateComponents([.year], from: birthDate, to: Date())
+                
+                if (age.year ?? 0) >= 18 {
+                    textView.text = "I, \(firstName.text!) \(surname.text!), currently living at \(address.text!) in the city of \(city.text!) do hereby accept the terms and conditions assignment.\n\nI am \(age.year ?? 0) and therefore able to accept the terms and conditions of this assignment"
+                    mylabel.text = "You successfully accepted this terms and conditions"
+                } else {
+                    mylabel.text = "You are not eligible to accept the terms and condition as you are under 18!"
+                }
+            } else {
+                mylabel.text = "Please enter valid birthdate in formate of dd/mm/yyyy"
+            }
         }
     }
 }
